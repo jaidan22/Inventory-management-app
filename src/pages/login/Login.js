8d2157bc06a74865c26f6f302d333e69bc3f88e5
mish027 <mish.02@yahoo.com>
2023-03-10 18:14:33 +0530

    test commit

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  return <div>
+    <div class="card-info">
+        <h2>DOLPHINS</h2>
+     
+        <p>Dolphins have a reputation for being friendly, but they are actually wild animals who should be treated with caution and respect. Interactions with people change dolphin behavior for the worse.</p>
+          
+         <a href="#">SHARE</a>
+      </div>
+    
+ </div>;
 };
 
 export default Login;
